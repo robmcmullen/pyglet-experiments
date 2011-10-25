f05a3455610fdfec9a220e916844757c83e9eeaa
Rob McMullen <robm@users.sourceforge.net>
2011-10-24 19:32:49 -0700

    Refactored drawing code into MenuRenderer

--- pyglet-main-window.py
+++ pyglet-main-window.py
@@ -7,17 +7,34 @@ import pyglet
 class MainWindow(pyglet.window.Window):
     def __init__(self, menu, width=1280, height=720):
         super(MainWindow, self).__init__(width, height)
-        self.cursor = 0
-        self.font_name = "Droid Sans"
-        self.font_size = 20
-        self.selected_font_size = 25
         self.menu = menu
-        
+        self.renderer = MenuRenderer(self, menu)
+    
+    def on_draw(self):
+        print "draw"
+        self.clear()
+        self.renderer.draw()
+    
+    def on_text_motion(self, motion):
+        print "here"
+        self.menu.process_motion(motion, self.renderer)
+        self.flip()
+
+
+class MenuRenderer(object):
+    def __init__(self, window, menu):
+        self.window = window
+        self.menu = menu
         self.load_fonts()
         self.compute_params()
     
+    def load_fonts(self):
+        self.font_name = "Droid Sans"
+        self.font_size = 20
+        self.selected_font_size = 25
+
     def compute_params(self):
-        self.width, self.height = self.get_size()
+        self.width, self.height = self.window.get_size()
         print self.width
         print self.height
         self.title_height = self.font_size + 10
@@ -25,12 +42,7 @@ class MainWindow(pyglet.window.Window):
         self.center = (self.height - self.title_height)/2
         self.items_in_half = (self.center - self.selected_font_size) / self.font_size
     
-    def load_fonts(self):
-        self.title_font = pyglet.font.load(self.font_name, self.font_size, bold=True, italic=False)
-    
-    def on_draw(self):
-        print "draw"
-        self.clear()
+    def draw(self):
         self.draw_title()
         self.draw_menu()
         #self.draw_detail()
@@ -43,10 +55,10 @@ class MainWindow(pyglet.window.Window):
                                   x=self.width/2, y=self.height - self.title_height,
                                   anchor_x='center', anchor_y='bottom')
         label.draw()
-    
+        
     def draw_menu(self):
         color = (0, 255, 0, 255)
-        text = self.menu.get_label(self.cursor)
+        text = self.menu.get_label(self.menu.cursor)
         # Render center item in larger font
         label = pyglet.text.Label(text,
                                   font_name=self.font_name,
@@ -56,8 +68,8 @@ class MainWindow(pyglet.window.Window):
         label.draw()
         
         y = self.center + self.selected_font_size
-        i = self.cursor - 1
-        limit = max(0, self.cursor - self.items_in_half)
+        i = self.menu.cursor - 1
+        limit = max(0, self.menu.cursor - self.items_in_half)
         while i >= limit:
             text = self.menu.get_label(i)
             label = pyglet.text.Label(text,
@@ -70,8 +82,8 @@ class MainWindow(pyglet.window.Window):
             i -= 1
             
         y = self.center - self.selected_font_size
-        i = self.cursor + 1
-        limit = min(self.menu.num_labels() - 1, self.cursor + self.items_in_half)
+        i = self.menu.cursor + 1
+        limit = min(self.menu.num_labels() - 1, self.menu.cursor + self.items_in_half)
         while i <= limit:
             text = self.menu.get_label(i)
             label = pyglet.text.Label(text,
@@ -83,32 +95,31 @@ class MainWindow(pyglet.window.Window):
             y -= self.font_size
             i += 1
     
-    def on_text_motion(self, motion):
-        print "here"
-        if motion == pyglet.window.key.MOTION_UP:
-            self.cursor -= 1
-        elif motion == pyglet.window.key.MOTION_PREVIOUS_PAGE:
-            self.cursor -= self.items_in_half
-        elif motion == pyglet.window.key.MOTION_DOWN:
-            self.cursor += 1
-        elif motion == pyglet.window.key.MOTION_NEXT_PAGE:
-            self.cursor += self.items_in_half
-        if self.cursor < 0:
-            self.cursor = 0
-        elif self.cursor >= self.menu.num_labels():
-            self.cursor = self.menu.num_labels() - 1
-            
-        self.flip()
 
 class Menu(object):
     def __init__(self):
-        pass
+        self.cursor = 0
     
     def get_label(self, index):
         return "Entry #%d" % index
     
     def num_labels(self):
         return 50
+
+    def process_motion(self, motion, renderer):
+        print "here"
+        if motion == pyglet.window.key.MOTION_UP:
+            self.cursor -= 1
+        elif motion == pyglet.window.key.MOTION_PREVIOUS_PAGE:
+            self.cursor -= renderer.items_in_half
+        elif motion == pyglet.window.key.MOTION_DOWN:
+            self.cursor += 1
+        elif motion == pyglet.window.key.MOTION_NEXT_PAGE:
+            self.cursor += renderer.items_in_half
+        if self.cursor < 0:
+            self.cursor = 0
+        elif self.cursor >= self.num_labels():
+            self.cursor = self.num_labels() - 1
 
 menu = Menu()
 window = MainWindow(menu)
