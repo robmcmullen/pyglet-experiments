cd43161ea1246d7dd4d9ff3d085db72cd442ddf4
Rob McMullen <robm@users.sourceforge.net>
2011-10-24 20:58:24 -0700

    Added simple menu that loads off the filesystem to find movies

--- pyglet-main-window.py
+++ pyglet-main-window.py
@@ -2,6 +2,8 @@
 
 # Simple main window test for a media player
 
+import os, sys, glob
+
 import pyglet
 
 class MainWindow(pyglet.window.Window):
@@ -107,9 +109,10 @@ class MenuRenderer(object):
         
 
 class Menu(object):
+    default_image = pyglet.image.load("artwork-not-available.png")
+    
     def __init__(self):
         self.cursor = 0
-        self.default_image = pyglet.image.load("artwork-not-available.png")
     
     def get_label(self, index):
         return "Entry #%d" % index
@@ -138,6 +141,97 @@ class Menu(object):
         elif self.cursor >= self.num_labels():
             self.cursor = self.num_labels() - 1
 
-menu = Menu()
-window = MainWindow(menu)
-pyglet.app.run()
+
+class MovieMenu(Menu):
+    video_extensions = ['.vob', '.mp4', '.avi', '.wmv', '.mov', '.mpg', '.mpeg', '.mpeg4', '.mkv', '.flv']
+    
+    def __init__(self, path):
+        super(MovieMenu, self).__init__()
+        self.videos = []
+        self.add_videos_in_path(path)
+        
+    def add_videos_in_path(self, dir):
+        videos = glob.glob(os.path.join(dir, "*"))
+        for video in videos:
+            valid = False
+            if os.path.isdir(video):
+                if not video.endswith(".old"):
+                    if self.exclude:
+                        match = self.exclude.search(video)
+                        if match:
+                            if self.verbose: print("Skipping dir %s" % video)
+                            continue
+                    print("Checking dir %s" % video)
+                    self.add_videos_in_path(video)
+            elif os.path.isfile(video):
+                print("Checking %s" % video)
+                for ext in self.video_extensions:
+                    if video.endswith(ext):
+                        valid = True
+                        print ("Found valid media: %s" % video)
+                        break
+                if valid:
+                    self.add_video(video)
+        self.videos.sort()
+    
+    def add_video(self, filename):
+        """Check to see if the filename is associated with a series
+        """
+        video = MplayerTarget(filename)
+        self.videos.append(video)
+
+    def get_label(self, index):
+        video = self.videos[index]
+        return video.title
+    
+    def get_detail_image(self, index):
+        video = self.videos[index]
+        return video.get_image()
+    
+    def get_details(self, index):
+        video = self.videos[index]
+        return video.details
+    
+    def num_labels(self):
+        return len(self.videos)
+
+
+class MplayerTarget(object):
+    def __init__(self, pathname):
+        self.fullpath = pathname
+        self.dirname, self.filename = os.path.split(pathname)
+        self.fileroot, self.fileext = os.path.splitext(self.filename)
+        self.title = self.fileroot.replace('_n_',' & ').replace('-s_','\'s ').replace('-t_','\'t ').replace('-m_','\'m ').replace('_',' ')
+        self.details = "Details for %s" % self.title
+        self.image = None
+    
+    def __cmp__(self, other):
+        return cmp(self.title, other.title)
+    
+    def get_image(self):
+        if self.image is None:
+            imagedir = os.path.join(self.dirname, ".thumbs")
+            for ext in [".jpg", ".png"]:
+                imagepath = os.path.join(imagedir, self.fileroot + ext)
+                print "checking %s" % imagepath
+                if os.path.exists(imagepath):
+                    self.image = pyglet.image.load(imagepath)
+                    print "loaded %s" % imagepath
+                    break
+            if self.image is None:
+                self.image = Menu.default_image
+        return self.image
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    elif os.path.exists("/remote/media2/movies"):
+        path = "/remote/media2/movies"
+    else:
+        path = None
+    if path:
+        menu = MovieMenu(path)
+    else:
+        menu = Menu()
+    window = MainWindow(menu)
+    pyglet.app.run()
